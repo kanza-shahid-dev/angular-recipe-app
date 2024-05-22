086c85b0a08af6e5f66e574c978e578d499825b0
Kanza Shahid <kanza.shahid@teo-intl.com>
2024-05-22 11:35:16 +0500

    file-structure-created

---
+++ src/app/recipes/recipe-list/reciper-item/reciper-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReciperItemComponent } from './reciper-item.component';
+
+describe('ReciperItemComponent', () => {
+  let component: ReciperItemComponent;
+  let fixture: ComponentFixture<ReciperItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReciperItemComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(ReciperItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/recipes/recipe-list/reciper-item/reciper-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reciper-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './reciper-item.component.html',
+  styleUrl: './reciper-item.component.scss'
+})
+export class ReciperItemComponent {
+
+}
